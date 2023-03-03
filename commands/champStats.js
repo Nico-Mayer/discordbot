22d73851b1aa8dbc933b053211aa3fde4eb645cc
Nico-Mayer <nico.m94@icloud.com>
2023-03-03 17:48:42 +0100

    string format for champ stats improved

--- commands/champStats.js
+++ commands/champStats.js
@@ -24,9 +24,42 @@ module.exports = {
 
 		embed.setColor('#fff000')
 		embed.setTitle(result[0].name)
-		embed.setDescription(
-			JSON.stringify(result[0].stats, null, 2).slice(1, -1)
-		)
+
+		const stats = `
+HP = ${result[0].stats.hp}
+HPLevel = ${result[0].stats.hpperlevel}
+
+MP = ${result[0].stats.mp}
+MPLevel = ${result[0].stats.mpperlevel}
+
+MoveSpeed = ${result[0].stats.movespeed}
+
+Armor = ${result[0].stats.armor}
+ArmorLevel = ${result[0].stats.armorperlevel}
+
+MagicResist = ${result[0].stats.spellblock}
+MagicResistLevel = ${result[0].stats.spellblockperlevel}
+
+AttackRange = ${result[0].stats.attackrange}
+
+HPregen = ${result[0].stats.hpregen}
+HPregenLevel = ${result[0].stats.hpregenperlevel}
+
+MPregen = ${result[0].stats.mpregen}
+MPregenLevel = ${result[0].stats.mpregenperlevel}
+
+Crit = ${result[0].stats.crit}
+CritLevel = ${result[0].stats.critperlevel}
+
+AttackDamage = ${result[0].stats.attackdamage}
+AttackDamageLevel = ${result[0].stats.attackdamageperlevel}
+
+AttackSpeed = ${result[0].stats.attackspeed}
+AttackSpeedLevel = ${result[0].stats.attackspeedperlevel}
+			`
+
+		//embed.setDescription(stats)
+		embed.setDescription('```javascript' + stats + '```')
 		embed.setThumbnail(
 			`http://ddragon.leagueoflegends.com/cdn/13.4.1/img/champion/${result[0].image.full}`
 		)
